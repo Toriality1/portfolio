ef23172340aec372dddcb9a7c9bcd1815d3349eb
Pedro Crelier <pcrelier@hotmail.com>
2025-08-07 21:45:33 -0300

    fix title animation jittering (hopefully)

--- features/AboutMe/components/Title.tsx
+++ features/AboutMe/components/Title.tsx
@@ -26,7 +26,7 @@ export const Title = ({
     <div className="flex flex-col text-center">
       <p className="mb-2 font-extralight text-accent opacity-50">{title}</p>
       <h1 className="title text-6xl font-bold text-accent">Pedro Crelier</h1>
-      <div className="relative my-2 space-y-2">
+      <div className="relative my-2 h-[1.75rem] space-y-2">
         <div className="mx-auto h-[1px] w-48 bg-accent" />
         <AnimatePresence mode="wait">
           <motion.p
