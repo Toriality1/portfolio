8e54b86de62ff0c5b75d1d960b297a49acd5e461
Pedro Crelier <pcrelier@hotmail.com>
2025-08-10 10:14:27 -0300

    finish blog section

--- features/Blog/index.tsx
+++ features/Blog/index.tsx
@@ -1,6 +1,8 @@
 import { Section } from "@/components/Section";
 import { SectionHeader } from "@/components/SectionHeader";
+import { useTranslations } from "next-intl";
 import { createElement } from "react";
+import { FiExternalLink } from "react-icons/fi";
 import Markdown from "react-markdown";
 
 const mockedData = `
@@ -10,22 +12,23 @@ Hello world
 `;
 
 async function getFeaturedPost() {
-    const post = await fetch(process.env.BLOG_URL! + "/api/featured", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
+  const post = await fetch(process.env.BLOG_URL! + "/api/featured", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (!post.ok) {
-      return mockedData
-    }
+  if (!post.ok) {
+    return mockedData;
+  }
 
-    return await post.json()
-
+  return await post.json();
 }
 
 export default async function Blog() {
+  const t = useTranslations("Blog");
+
   const headingComponents = Object.fromEntries(
     Array.from({ length: 6 }, (_, i) => {
       const tag = `h${i + 1}`;
@@ -37,18 +40,30 @@ export default async function Blog() {
     }),
   );
 
-  const post = await getFeaturedPost()
+  const post = await getFeaturedPost();
 
   return (
     <Section id="Blog" color="bg-black" className="space-y-16 text-white">
       <SectionHeader>Blog</SectionHeader>
-      <div className='mx-auto w-fit' >
+      <div className="mx-auto w-fit">
         <h4 className="title text-2xl uppercase opacity-50">
-          Featured blog post:
+        {t("featured")}
         </h4>
-        <article className="prose prose-invert">
-          <Markdown components={headingComponents}>{post.content}</Markdown>
-        </article>
+        <div className="relative h-[80vh] overflow-hidden">
+          <div className="absolute bottom-0 flex h-1/2 w-full justify-end bg-gradient-to-t from-black from-10% to-transparent">
+            <button className="group m-auto mb-10 w-fit translate-y-10 border-2 border-neutral-600/50 p-1 transition hover:border-neutral-800/50 disabled:pointer-events-none disabled:opacity-70">
+              <p className="title flex items-center gap-2 bg-neutral-600 px-4 py-2 text-lg font-bold uppercase tracking-wide text-white transition group-hover:bg-neutral-800">
+                <span className="-translate-y-0.5">
+                  <FiExternalLink />
+                </span>
+                <span>{t("read")}</span>
+              </p>
+            </button>
+          </div>
+          <article className="prose prose-invert">
+            <Markdown components={headingComponents}>{post.content}</Markdown>
+          </article>
+        </div>
       </div>
     </Section>
   );
