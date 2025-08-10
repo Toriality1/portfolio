19f1976febee736c5a8217ac9d0eec6ea175e218
Pedro Crelier <pcrelier@hotmail.com>
2025-08-10 13:53:50 -0300

    add external link to blog post

--- features/Blog/index.tsx
+++ features/Blog/index.tsx
@@ -1,6 +1,7 @@
 import { Section } from "@/components/Section";
 import { SectionHeader } from "@/components/SectionHeader";
 import { useTranslations } from "next-intl";
+import Link from "next/link";
 import { createElement } from "react";
 import { FiExternalLink } from "react-icons/fi";
 import Markdown from "react-markdown";
@@ -51,14 +52,14 @@ export default async function Blog() {
         </h4>
         <div className="relative h-[80vh] overflow-hidden">
           <div className="absolute bottom-0 flex h-1/2 w-full justify-end bg-gradient-to-t from-black from-10% to-transparent">
-            <button className="group m-auto mb-10 w-fit translate-y-10 border-2 border-neutral-600/50 p-1 transition hover:border-neutral-800/50 disabled:pointer-events-none disabled:opacity-70">
+            <Link target="_blank" href={process.env.BLOG_URL! + "/posts/" + post.slug} className="group m-auto mb-10 w-fit translate-y-10 border-2 border-neutral-600/50 p-1 transition hover:border-neutral-800/50 disabled:pointer-events-none disabled:opacity-70">
               <p className="title flex items-center gap-2 bg-neutral-600 px-4 py-2 text-lg font-bold uppercase tracking-wide text-white transition group-hover:bg-neutral-800">
                 <span className="-translate-y-0.5">
                   <FiExternalLink />
                 </span>
                 <span>{t("read")}</span>
               </p>
-            </button>
+            </Link>
           </div>
           <article className="prose prose-invert">
             <Markdown components={headingComponents}>{post.content}</Markdown>
